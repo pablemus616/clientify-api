2781b59c0bc20e9a2fef5be8c1b62b367898c801
pablemus616 <lemuspablo300@gmail.com>
2026-05-25 16:41:01 -0600

    fix method not supported handler

--- src/main/java/com/nihil/clientifyapi/config/GlobalExceptionFilter.java
+++ src/main/java/com/nihil/clientifyapi/config/GlobalExceptionFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -59,5 +60,15 @@ public class GlobalExceptionFilter
                 request.getRequestURI()
         );
         return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(er);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ExceptionResponse> handleGlobalHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex, HttpServletRequest req){
+        ExceptionResponse er = new ExceptionResponse(
+                false,
+                ex.getMessage(),
+                req.getRequestURI()
+        );
+        return ResponseEntity.status(400).body(er);
     }
 }
